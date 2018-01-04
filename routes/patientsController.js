ebb3b4511afa1c64d8d9c5db93175c2e3526b4cf
tonycimaglia <tonycimaglia@gmail.com>
2018-01-04 15:53:56 -0500

    route is working but database is not populating page

--- routes/patientsController.js
+++ routes/patientsController.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 const User = require('../db/models/User')
 
+
 router.get('/:patientId', (request, response) => {
     const userId = request.params.userId
     const patientId = request.params.patientId
 
     User.findById(userId)
         .then((user) => {
-            const patient = user.patients.id(storeId)
+            const patient = user.patients.id(patientId)
             response.render('patients/show', {
                 userId,
                 patient,
