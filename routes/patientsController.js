385c83a72b56ecbbf48407968df5d7c7d0de6da3
tonycimaglia <tonycimaglia@gmail.com>
2018-01-05 16:23:00 -0500

    created a create new patient route

--- routes/patientsController.js
+++ routes/patientsController.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 const User = require('../db/models/User')
 
+// show a specific patient
 
 router.get('/:patientId', (request, response) => {
     const userId = request.params.userId
@@ -19,6 +20,32 @@ router.get('/:patientId', (request, response) => {
             console.log(error)
         })
 })
+
+// new patient route
+
+router.get('/new', (request, response) => {
+    response.render('patients/new', {
+      pageTitle: 'New Patient'
+    })
+  })
+
+// router.post('/', (request, response) => {
+//     const userId = request.params.userId
+//     const newPatient = request.body
+
+//     User.findById(userId)
+//         .then((user) => {
+//             user.patients.push(newPatient)
+//             return user.save()
+//         })
+//         .then(() => {
+//             response.redirect(`/users/${userId}/patients`)
+//         })
+//         .catch((error) => {
+//             console.log(error)
+//         })
+
+// })
 
 module.exports = router
 
