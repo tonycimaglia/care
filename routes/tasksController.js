3d5bc0a53bf184ece6e88662d013173663d4df37
tonycimaglia <tonycimaglia@gmail.com>
2018-01-06 18:38:31 -0500

    did a little styling

--- routes/tasksController.js
+++ routes/tasksController.js
@@ -6,17 +6,34 @@ const User = require('../db/models/User')
 router.get('/new', (request, response) => {
     const userId = request.params.userId
     const patientId = request.params.patientId
-    console.log("Am I getting here?")
+    
     User.findById(userId)
-      .then((user) => {
-        const patient = user.patients.id(patientId)
-  
-        response.render('tasks/new', {
-          userId,
-          patient,
-          pageTitle: 'New Task'
+        .then((user) => {
+            const patient = user.patients.id(patientId)
+
+            response.render('tasks/new', {
+                userId,
+                patient,
+                pageTitle: 'New Task'
+            })
         })
-      })
-  })
+})
+
+router.post('/', (request, response) => {
+    const userId = request.params.userId
+    const patientId = request.params.patientId
+    const newTask = request.body
+
+    User.findById(userId)
+        .then((user) => {
+            const patient = user.patients.id(patientId)
+            patient.tasks.push(newTask)
+            console.log(patient.tasks)
+            return user.save()
+        })
+        .then(() => {
+            response.redirect(`/users/${userId}/patients/${patientId}`)
+        })
+})
 
 module.exports = router;
